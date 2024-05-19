c9c5e941e916f644414329f64aeecec2e3131a7f
Julian Bieber <j.bieber@patagona.de>
2024-05-19 20:06:39 +0200

    save document on changes

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/tliron/commonlog"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
@@ -14,6 +16,9 @@ const lsName = "glsl_lsp_go"
 var version string = "0.0.1"
 var handler protocol.Handler
 
+var documentMutex sync.Mutex
+var document string
+
 func main() {
 	path := "glsl_lsp.log"
 	commonlog.Configure(2, &path)
@@ -23,6 +28,9 @@ func main() {
 		Initialize:                 initialize,
 		Shutdown:                   shutdown,
 		TextDocumentDocumentSymbol: symbolProvider,
+		TextDocumentDidOpen:        documentOpen,
+		TextDocumentDidSave:        documentSave,
+		TextDocumentDidChange:      documentChange,
 	}
 
 	server := server.NewServer(&handler, lsName, true)
@@ -36,6 +44,7 @@ func initialize(context *glsp.Context, params *protocol.InitializeParams) (any, 
 	capabilities := handler.CreateServerCapabilities()
 
 	capabilities.DocumentSymbolProvider = protocol.DocumentSymbolOptions{}
+	capabilities.TextDocumentSync = protocol.TextDocumentSyncKindFull
 
 	return protocol.InitializeResult{
 		Capabilities: capabilities,
@@ -54,4 +63,22 @@ func symbolProvider(context *glsp.Context, params *protocol.DocumentSymbolParams
 	_ = params
 	var symbols []protocol.DocumentSymbol
 	return symbols, nil
+}
+
+func documentOpen(context *glsp.Context, params *protocol.DidOpenTextDocumentParams) error {
+	documentMutex.Lock()
+	defer documentMutex.Unlock()
+	document = params.TextDocument.Text
+	return nil
+}
+
+func documentChange(context *glsp.Context, params *protocol.DidChangeTextDocumentParams) error {
+	documentMutex.Lock()
+	defer documentMutex.Unlock()
+	document = params.ContentChanges[0].(string)
+	return nil
+}
+
+func documentSave(context *glsp.Context, params *protocol.DidSaveTextDocumentParams) error {
+	return nil
 }
