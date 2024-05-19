298e9e7c99826ac33959e20f3d1e1c5fd4941e13
Julian Bieber <julian.bieber.1992@gmail.com>
2024-05-20 01:33:34 +0200

    tmp

--- main.go
+++ main.go
@@ -18,11 +18,28 @@ var handler protocol.Handler
 
 var documentMutex sync.Mutex
 var document string
+var functions []FunctionDef
+var structs []StructDef
+var builtinTypes []string
+
+type FunctionDef struct {
+	Name  string
+	Range protocol.Range
+}
+
+type StructDef struct {
+	Name  string
+	Range protocol.Range
+}
 
 func main() {
 	path := "glsl_lsp.log"
 	commonlog.Configure(2, &path)
 	commonlog.NewInfoMessage(0, "Startup").Send()
+
+	builtinTypes = make([]string, 5)
+	// https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)
+	builtinTypes[0] = ""
 
 	handler = protocol.Handler{
 		Initialize:                 initialize,
@@ -62,6 +79,36 @@ func shutdown(context *glsp.Context) error {
 func symbolProvider(context *glsp.Context, params *protocol.DocumentSymbolParams) (any, error) {
 	_ = params
 	var symbols []protocol.DocumentSymbol
+	// prealloc with make
+	symbol := protocol.DocumentSymbol{
+		Name:       "some symbol",
+		Detail:     nil,
+		Kind:       protocol.SymbolKindFunction,
+		Tags:       []protocol.SymbolTag{},
+		Deprecated: nil,
+		Range: protocol.Range{
+			Start: protocol.Position{
+				Line:      0,
+				Character: 0,
+			},
+			End: protocol.Position{
+				Line:      0,
+				Character: 0,
+			},
+		},
+		SelectionRange: protocol.Range{
+			Start: protocol.Position{
+				Line:      0,
+				Character: 0,
+			},
+			End: protocol.Position{
+				Line:      0,
+				Character: 0,
+			},
+		},
+		Children: []protocol.DocumentSymbol{},
+	}
+	symbols = append(symbols, symbol)
 	return symbols, nil
 }
 
